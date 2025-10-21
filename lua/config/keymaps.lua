f79fc3711f7e5d056cb93d5af86242da1d6daef4
Eduard Llado Merino <elladmerino@microsoft.com>
2025-10-21 08:11:40 +0000

    Restore 's' functionality

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Tmux compatibility
 vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { silent = true })
+
+-- Remove plugin mapping for 's'
+vim.keymap.del({ "n", "x", "o" }, "s")
+
+-- Restore original Vim behavior for 's'
+vim.keymap.set("n", "s", "s", { noremap = true })
+
